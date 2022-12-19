fc99bdf3724d187bd47ec6a2c550e012f8778b3e
Andresha <lakshshaw570@gmail.com>
2022-12-19 13:20:19 +0530

    added nav close btn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 
 $(".fa-bars").click(()=>{
    $("ul").css({"right":"0"})
+   $(".fa-bars").css("display", "none");
+   $(".fa-xmark").css("display", "block");
+})
+$(".fa-xmark").click(()=>{
+   $(".fa-bars").css("display", "block");
+   $(".fa-xmark").css("display", "none");  
+   $("ul").css({"right":"100%"})
 })
